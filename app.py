0b871fad1124cd6707351feabcc048259ff2db72
vibhuti2023 <129948528+vibhuti2023@users.noreply.github.com>
2024-05-24 19:44:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('CI CD PIPELINE FOR ML MODEL!')
+st.title('Rahul project!')
 
 with st.sidebar:
     st.header('Data requirements')
